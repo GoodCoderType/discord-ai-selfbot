5b5e900bf8f44907caf4327d1aa6f7e162629f85
GoodCoderType <goodgamerontop@gmail.com>
2025-02-07 16:39:20 +0500

    Update bot.py Addec Checks

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# this part called imports
 try:
     import discord
     from discord.ext import commands
@@ -13,23 +12,20 @@ except:
 import asyncio
 import random
 import json
-# import ends
-# setup bot object
+
 bot =commands.Bot(
     command_prefix="!",
     self_bot=True
 )
 
-# read config
+
 with open("config.json") as f:
     config = json.load(f)
 
-# on ready when bot is ready to do things
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-# function to get response from ai
 async def ai(history,username, message):
     prompt = f"Message History For Context : {history}\nPrompt Author {username} Original Prompt {message}"
     try:
@@ -48,13 +44,26 @@ async def ai(history,username, message):
     except Exception as e:
         return f"Error: {str(e)} 🔥🚨😭"
 
-# on message. when some one sends messages this event triggers
+async def is_reply(msg: discord.Message):
+    if msg.reference:
+        try:
+            ref_msg = await msg.channel.fetch_message(msg.reference.message_id)
+            return ref_msg.author == bot.user
+        except (discord.NotFound, discord.Forbidden, discord.HTTPException):
+            return False
+    return False
+
 @bot.event
 async def on_message(msg: discord.Message):
-    if msg.channel.id in list(config.get("channel_ids")) and msg.author != bot.user and not msg.author.bot:
+    if (
+        msg.channel.id in config.get("channel_ids") and
+        msg.author != bot.user and
+        not msg.author.bot and
+        (bot.user in msg.mentions or await is_reply(msg))
+    ):
         async with msg.channel.typing():
             channel_history = []
-            async for user_msg in msg.channel.history(limit=20):
+            async for user_msg in msg.channel.history(limit=7):
                 if user_msg.author != bot.user:
                     channel_history.append({
                         "username": user_msg.author.display_name,
@@ -62,12 +71,10 @@ async def on_message(msg: discord.Message):
                     })
             response = await ai(channel_history,msg.author.display_name, msg.content)
 
-            funny_reactions = ["😂", "🤣", "😭", "💀", "👏","🤡"]
+            funny_reactions = ["😂", "🤣", "😭", "💀", "👏","🤡","🤗"]
             await msg.add_reaction(random.choice(funny_reactions))
-            
             
             await msg.reply(
                 content=f"{msg.author.mention}\n[Ai From Mars] : {response}"
             )
-# run bot
 bot.run(config.get("usertoken"))
